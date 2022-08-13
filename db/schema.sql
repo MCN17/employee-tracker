acacabc24a81b536fc6f8b2c18cb05987df4ac45
Marty <martynolan17@gmail.com>
2022-08-13 11:19:17 -0400

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,5 +12,13 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER, 
+    manager_id INTEGER
 );
 
