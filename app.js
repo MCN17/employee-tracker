bb270102f73acd4cf39ed8c35ab07ed96f723d76
Marty <martynolan17@gmail.com>
2022-08-13 15:43:40 -0400

    added constraints to tables/added opening prompt

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 const consoleTable = require('console.table');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const mysql = require('mysql2');
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'newnewpassword',
+      database: 'companyDatabase'
+    },
+    console.log("test")
+  );
+
+  const promptOwner = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Please choose one of the following options:",
+            choices:["View all Departments", "View all Roles", "View all Employees", "Add a Department", "Add a Role", "Update an Employee Role"]
+        }
+    ])
+  }
+
+promptOwner();
--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,9 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER
+    department_id INTEGER,
+
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -19,6 +21,10 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER, 
-    manager_id INTEGER
+    manager_id INTEGER,
+
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
 );
 
