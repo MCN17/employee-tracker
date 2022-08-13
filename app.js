98a84efaf1acba9a7f5cc5c658eaa63dd9534a44
Marty <martynolan17@gmail.com>
2022-08-13 16:53:15 -0400

    added functions to view all departments/roles

--- app.js
+++ app.js
@@ -19,11 +19,42 @@ const db = mysql.createConnection(
     return inquirer.prompt([
         {
             type: "list",
-            name: "role",
+            name: "options",
             message: "Please choose one of the following options:",
             choices:["View all Departments", "View all Roles", "View all Employees", "Add a Department", "Add a Role", "Update an Employee Role"]
         }
     ])
+    .then(userChoice => {
+      switch(userChoice.options) {
+        case "View all Departments":
+          viewDepartments();
+          break;
+        case "View all Roles":
+          viewRoles();
+          break;  
+      }
+    })
   }
+
+const viewDepartments = () => {
+  db.query(`SELECT * FROM departments;`, function(err, rows) {
+    console.table(rows);
+    promptOwner();
+  });
+};
+
+const viewRoles = () => {
+  db.query(`SELECT * FROM role;`, function(err, rows) {
+    console.table(rows);
+    promptOwner();
+  });
+};
+
+// const viewEmployees = () => {
+//   db.query(`SELECT * FROM employee;`, function(err, rows) {
+//     console.table(rows);
+//   });
+// };
+
 
 promptOwner();
